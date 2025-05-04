c319488b57b2d29c564f264741800f7b9569c87e
AlbaGomesG <162628812+AlbaGomesG@users.noreply.github.com>
2025-05-04 15:43:06 -0300

    Segurança com Api Key

--- src/routes/publishersRoutes.js
+++ src/routes/publishersRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const publishersController = require("../controllers/publishersController.js");
+const apiKeyMiddleware = require("../config/apiKey.js");
+
+router.use(apiKeyMiddleware);
 
 router.get("/", publishersController.getAllPublishers);
 router.get("/:id", publishersController.getPublisher);
